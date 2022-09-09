c194b2019c3bf5ebb84dbe276ea9f72835dbb02a
mare.stephen <mare.stephen@gmail.com>
2022-09-09 07:54:13 +0200

    Changes made to the js file

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.container');         // will select just one
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');      //will select all and put them into a node / array format
+//will select all and put them into a node / array format.
+// also there should be no space between :not() otherwise it will raise an exception
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');      
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
